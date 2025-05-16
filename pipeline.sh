85cff0cc42234b34c05c45221e1ece65b3a3a65c
myeonggil <echosoul1994@gmail.com>
2025-05-16 14:05:23 +0900

    [CHORE] docker build and push workflow

--- pipeline.sh
+++ pipeline.sh
@@ -6,6 +6,7 @@ docker login
 docker build --platform=linux/amd64 -t otc .
 
 # tag local to remote
+docker tag otc mgju/open-template-chatbot:latest
 
 # push docker hub(private repo)
 docker push mgju/open-template-chatbot:latest
