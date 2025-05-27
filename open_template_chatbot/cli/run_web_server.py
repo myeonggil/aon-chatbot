886f7839f068432f9a806a1c3ed9e86c8e86b2c0
myeonggil <echosoul1994@gmail.com>
2025-05-27 14:10:59 +0900

    [UPDATE] typer run streamlit and slackbot

---
+++ open_template_chatbot/cli/run_web_server.py
@@ -0,0 +1,19 @@
+import typer
+import subprocess
+
+
+web_server_app = typer.Typer(name='streamlit')
+@web_server_app.command(name='run')
+def startup_gui(
+    browser_gatherUsageStats: str = typer.Option('false', "--browser.gatherUsageStats", help="gather usage stats"),
+    server_headless: str = typer.Option('true', "--server.headless", help="server without head"),
+    server_enableXsrfProtection: str = typer.Option('true', "--server.enableXsrfProtection", help="server without head"),
+    server_enableCORS: str = typer.Option('false', "--server.enableCORS", help="server without head"),
+    server_address: str = typer.Option('0.0.0.0', "--server.address", help="server without head"),
+    server_port: str = typer.Option('8501', "--server.port", help="server without head"),
+):
+    run_command = ["streamlit", "run", "--browser.gatherUsageStats", browser_gatherUsageStats,
+        "--server.headless", server_headless, "--server.enableXsrfProtection", server_enableXsrfProtection,
+        "--server.enableCORS", server_enableCORS, "--server.address", server_address,
+        "--server.port", server_port, "open_template_chatbot/interaction/chatbot_with_gui.py"]
+    subprocess.run(run_command)
--- open_template_chatbot/repository.py
+++ open_template_chatbot/repository.py
@@ -1,10 +1,13 @@
+from pymongo import AsyncMongoClient
+
 from open_template_chatbot.interfaces.chat_repository_interface import IChatRepository
 from open_template_chatbot.interfaces.llm_repository_interface import ILLMRepository
 
 
-
-
 class LLMRepository(ILLMRepository):
+
+    def __init__(self, client: AsyncMongoClient):
+        self.client = client
 
     async def insert_pdf_docs(self, docs):
         return await super().insert_pdf_docs(docs)
